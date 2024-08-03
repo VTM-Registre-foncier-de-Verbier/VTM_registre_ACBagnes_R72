02513685f7341befe38357776adbb4cc8018e168
Christine Payot <113700920+Ch365HN@users.noreply.github.com>
2024-08-03 16:05:30 +0200

    Update subseg.sh

--- submission_scripts/subseg.sh
+++ submission_scripts/subseg.sh
@@ -4,7 +4,7 @@
 #SBATCH --gpus=1
 #SBATCH --output=kraken-%j.out
 #SBATCH --mem=12GB
-#SBATCH --ntasks=12
+#SBATCH --ntasks-per-node=12
 #SBATCH --gres=gpu:1,VramPerGpu:24GB
 
 module load CUDA/11.8.0 GCCcore/11.2.0 Python/3.9.6
