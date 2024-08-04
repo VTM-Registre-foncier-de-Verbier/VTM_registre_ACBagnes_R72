60ed1140b7557edfdd5a30ad8084d07024207ffe
Ch365HN <christinepayot@yahoo.fr>
2024-08-04 20:40:49 +0200

    Mis slt 20 fichiers dans dossier Data

--- submission_scripts/subrec.sh
+++ submission_scripts/subrec.sh
@@ -4,14 +4,14 @@
 #SBATCH --gpus=1
 #SBATCH --output=kraken-%j.out
 #SBATCH --mem=12GB
-#SBATCH --ntasks=12
+#SBATCH --ntasks-per-node=12
 #SBATCH --gres=gpu:1,VramPerGpu:24GB
 
 module load CUDA/11.8.0 GCCcore/11.2.0 Python/3.9.6
 source ~/kraken-env/bin/activate
 
 OUTPUT_NAME="output_name"
-XML_FOLDER="/home/users/p/payotch2/VTM_Depot_entrainement/data"
+XML_FOLDER="/home/users/p/payotch2/VTM_registre_ACBagnes_R72/data"
 
 echo "KETOS training"
 srun ketos train -o $OUTPUT_NAME -f alto -d cuda:0 "${XML_FOLDER}/*.xml" --workers 4
