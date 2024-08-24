884d1a5598491cd73a255037488de0df3648e557
Christine Payot <113700920+Ch365HN@users.noreply.github.com>
2024-08-24 16:36:22 +0200

    Add files via upload

---
+++ submission_scripts/subseg.sh
@@ -0,0 +1,15 @@
+#!/bin/env bash
+#SBATCH --partition=shared-gpu
+#SBATCH --time=02:00:00
+#SBATCH --gpus=1
+#SBATCH --output=kraken-%j.out
+#SBATCH --mem=12GB
+#SBATCH --ntasks-per-node=12
+#SBATCH --gres=gpu:1,VramPerGpu:24GB
+
+module load CUDA/11.8.0 GCCcore/11.2.0 Python/3.9.6
+source ~/kraken-env/bin/activate
+
+
+echo "KETOS training"
+srun ketos segtrain -t train.txt
