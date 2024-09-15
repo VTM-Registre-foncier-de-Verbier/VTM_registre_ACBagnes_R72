9778dc9732fe90eab91a5d06f93e03477c441042
Ch365HN <christinepayot@yahoo.fr>
2024-09-15 20:41:39 +0200

    Ajout des modèles et scripts

--- submission_scripts/subrec.sh
+++ submission_scripts/subrec.sh
@@ -1,10 +1,10 @@
 #!/bin/env bash
 #SBATCH --partition=shared-gpu
-#SBATCH --time=02:00:00
+#SBATCH --time=08:00:00
 #SBATCH --gpus=1
 #SBATCH --output=kraken-%j.out
-#SBATCH --mem=12GB
-#SBATCH --ntasks-per-node=12
+#SBATCH --mem=24GB
+#SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1,VramPerGpu:24GB
 
 module load CUDA/11.8.0 GCCcore/11.2.0 Python/3.9.6
@@ -12,4 +12,4 @@ source ~/kraken-env/bin/activate
 
 
 echo "KETOS training"
-srun ketos train -t train.txt
+srun ketos train --load output_8_19.mlmodel -f alto -d cuda:0 -t train.txt -e val.txt --resize union
--- submission_scripts/subseg.sh
+++ submission_scripts/subseg.sh
@@ -1,11 +1,12 @@
 #!/bin/env bash
 #SBATCH --partition=shared-gpu
-#SBATCH --time=02:00:00
+#SBATCH --time=08:00:00
 #SBATCH --gpus=1
 #SBATCH --output=kraken-%j.out
-#SBATCH --mem=12GB
-#SBATCH --ntasks-per-node=12
+#SBATCH --mem=24GB
+#SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1,VramPerGpu:24GB
+
 
 module load CUDA/11.8.0 GCCcore/11.2.0 Python/3.9.6
 source ~/kraken-env/bin/activate
